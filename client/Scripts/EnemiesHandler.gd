0237f87dcc97783dcb7fdd3c3b22d38dd21ad41e
Arthur Ramos <arthur.rlima19@gmail.com>
2023-09-21 04:37:51 -0300

    why doesnt work? idk

--- client/Scripts/EnemiesHandler.gd
+++ client/Scripts/EnemiesHandler.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var EnemyTemplateScene: PackedScene
 
 var last_world_state_timestamp = 0
+var world_state_buffer = []
+const INTERPOLATION_OFFSET = 50
 
 func spawn(player_id, spawn_position):
 	if multiplayer.get_unique_id() == player_id:
@@ -22,20 +24,42 @@ func despawn(player_id):
 		get_node(str(player_id)).queue_free()
 
 func update(world_state):
-	# Buffer
-	# Interpolation
-	# Extrapolation
-	# Rubber banding
 	if world_state['T'] < last_world_state_timestamp:
 		return
+	Pry.log("receiving world_state")
 	last_world_state_timestamp = world_state['T']
-	world_state.erase('T') # needed later for inter/extrapolation
-	world_state.erase(multiplayer.get_unique_id()) # maybe needed later for anti...
-	for player_id in world_state.keys():
-		if has_node(str(player_id)):
-			get_node(str(player_id)).move_player(world_state[player_id]['P'])
+	world_state_buffer.append(world_state)
+	
+func _physics_process(delta):
+	var render_time = Time.get_unix_time_from_system() - INTERPOLATION_OFFSET
+	if world_state_buffer.size() <= 1:
+		return
+	while world_state_buffer.size() > 2 and render_time > world_state_buffer[1].T:
+		world_state_buffer.remove(0)
+	#Pry.log(world_state_buffer)
+	var time_elapsed_render = float(render_time - world_state_buffer[0]['T'])
+	var time_elapsed_states = float(world_state_buffer[1]['T'] - world_state_buffer[0]['T'])
+	var interpolation_factor = time_elapsed_render / time_elapsed_states
+	for player_id in world_state_buffer[1].keys():
+		if str(player_id) == 'T': # ignore timestamp key
+			Pry.log("ignore timestamp key")
+			continue
+		elif player_id == multiplayer.get_unique_id(): # ignore yourselfs
+			Pry.log("ignore yourselfs")
+			continue
+		elif not world_state_buffer[0].has(player_id): # ignore just logged players
+			Pry.log("logged players")
+			continue
+		elif has_node(str(player_id)):
+			Pry.log("MOVE PLAYER")
+			var new_position = lerp(world_state_buffer[0][player_id]['P'], world_state_buffer[1][player_id]['P'], interpolation_factor)
+			get_node(str(player_id)).move_player(new_position)
 		else:
-			spawn(player_id, world_state[player_id]['P'])
+			Pry.log("SPAWN PLAYER")
+			spawn(player_id, world_state_buffer[1][player_id]['P'])
+	
+	
+	
 	
 	
 	
